8ba192d5e1125ca7dcbdbb6ff5ea14ac0def5582
Clownacy <fred.arnold101@googlemail.com>
2015-06-08 22:37:28 +0100

    Outputting errors and listing file

--- buildROM.bat
+++ buildROM.bat
@@ -3,7 +3,7 @@ echo Build started...
 
 call BuildZ80.bat
 
-asm68k /o ae- /p S2NA.asm,S2NA.bin
+asm68k /k /p /o ae- S2NA.asm, S2NA.bin >errors.txt, , S2NA.lst
 
 IF NOT EXIST S2NA.bin goto LABLERR
 rem rompad S2NA.bin 255 0
