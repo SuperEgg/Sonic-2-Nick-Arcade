a4dbd2572d06c7de6a9bff15b054429a850e004e
Clownacy <fred.arnold101@googlemail.com>
2015-06-14 17:04:24 +0100

    Shutting AS up

--- BuildZ80.bat
+++ BuildZ80.bat
@@ -5,7 +5,7 @@ set USEANSI=n
 md output
 md output\uncompressed
 md output\compressed
-as\asl -gnuerrors -c -A -xx smps.asm
+as\asl -gnuerrors -c -A -xx -q smps.asm
 as\p2bin smps.p output\uncompressed\smpsbuilt.bin -r 0x-0x
 derecmp kc output\uncompressed output\compressed
 copy output\compressed\smpsbuilt.bin z80.bin
